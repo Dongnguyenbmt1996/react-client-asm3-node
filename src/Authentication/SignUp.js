a15fcd7eec569d2e959e274562c6b75d06a1168e
Dongnguyenbmt1996 <131173013+Dongnguyenbmt1996@users.noreply.github.com>
2025-04-13 23:16:54 +0700

    Update SignUp.js

--- src/Authentication/SignUp.js
+++ src/Authentication/SignUp.js
@@ -23,7 +23,7 @@ export default function SignUp() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/auth/signup", {
+      const res = await fetch("https://lab03-node.onrender.com/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
